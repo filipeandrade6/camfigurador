09d667f3594aa713087ab2ee9a84d949970a6d95
Filipe Andrade <filipe.engenharia42@gmail.com>
2022-07-28 17:46:07 -0300

    configuration

--- dahua.go
+++ dahua.go
@@ -10,38 +10,62 @@ import (
 var baseUrl = "/cgi-bin/configManager.cgi?action=setConfig&"
 
 type Configurator struct {
-	Condition string
-	Video     string
-	Network   string
-	PTZ       string
-	Date      string
-	Account   string
+	Condition    string
+	Video        string
+	Network      string
+	PTZ          string
+	Date         string
+	Account      string
+	UserAdd      string
+	UserAddSigeo string
+	ChangePass   string
 }
 
-func NewDahuaConfigurator() Configurator {
-	return Configurator{
-		Condition: "VideoInBacklight[0][2].Mode=WideDynamic&VideoInBacklight[0][2].WideDynamicRange=20&VideoInExposure[0][2].Compensation=20&VideoInDayNight[0][2].Mode=Color&VideoInMode[0].Config[0]=2",
+const (
+	condition = iota
+	video
+	network
+	date
+	userAdd
+	userAddSigeo
+	changePass
+)
 
-		Video:   "Encode[0].MainFormat[0].Video.Compression=H.264&Encode[0].MainFormat[0].Video.Profile=Main&Encode[0].MainFormat[0].Video.resolution=1920x1080&Encode[0].MainFormat[0].Video.Height=1080&Encode[0].MainFormat[0].Video.Width=1920&Encode[0].MainFormat[0].Video.Width=1920&Encode[0].MainFormat[0].Video.FPS=16&Encode[0].MainFormat[0].Video.BitRateControl=CBR&Encode[0].MainFormat[0].Video.BitRate=4096&Encode[0].MainFormat[0].Video.GOP=32&Encode[0].MainFormat[1].VideoEnable=False&ChannelTitle[0].Name={SSP/DF - localização - PONTO}",
-		Network: "Network.Hostname={nome da câmera}&Network.eth0.IPAddress={ip}&Network.eth0.DefaultGateway={gateway}&Network.eth0.SubnetMask={mascara de subrede}",
-		Date:    "NTP.TimeZone=22&NTP.TimeZoneDesc=Brasilia&NTP.Address=10.92.0.54&NTP.Enable=true", // OK
-		// 	Account:
-		// 		"url base":                           "http://<ip-address>/cgi-bin/userManager.cgi?action=", // TODO
-		// 		"add account":                        "addUser&user.Name={usuario}&us er.Password={senha}&user.Group={user|admin}&user.Sharable=true&user.Reserved=false",
-		// 		"change admin password":              "modifyPassword&name=admin&pwd={senha}&pwdOld={senha-antiga}",
-		// 		"onvif user / add account":           "<<ACHO QUE NÃO TEM>>", // TODO
-		// 		"onvif user / change admin password": "modifyPasswordByManager&userName=admin&pwd{senha}&managerName={admin}&managerPwd={senha}&accountType=1",
-		// }
+func NewDahuaConfigurator() map[int]string {
+	return map[int]string{
+		condition:    "/cgi-bin/configManager.cgi?action=setConfig&VideoInBacklight[0][2].Mode=WideDynamic&VideoInBacklight[0][2].WideDynamicRange=20&VideoInExposure[0][2].Compensation=20&VideoInDayNight[0][2].Mode=Color&VideoInMode[0].Config[0]=2",
+		video:        "/cgi-bin/configManager.cgi?action=setConfig&Encode[0].MainFormat[0].Video.Compression=H.264&Encode[0].MainFormat[0].Video.Profile=Main&Encode[0].MainFormat[0].Video.resolution=1920x1080&Encode[0].MainFormat[0].Video.Height=1080&Encode[0].MainFormat[0].Video.Width=1920&Encode[0].MainFormat[0].Video.FPS=16&Encode[0].MainFormat[0].Video.BitRateControl=CBR&Encode[0].MainFormat[0].Video.BitRate=4096&Encode[0].MainFormat[0].Video.GOP=32&ChannelTitle[0].Name=%localizacao%&table.VideoWidget[0].FontSize=32&table.VideoWidget[0].ChannelTitle.FrontColor[0]=255&table.VideoWidget[0].ChannelTitle.FrontColor[1]=255&table.VideoWidget[0].ChannelTitle.FrontColor[2]=0&table.VideoWidget[0].ChannelTitle.FrontColor[3]=0",
+		network:      "/cgi-bin/configManager.cgi?action=setConfig&Network.Hostname=%patrimonio%&Network.eth0.IPAddress=%ip%&Network.eth0.DefaultGateway=%gateway%&Network.eth0.SubnetMask=%mascara%",
+		date:         "/cgi-bin/configManager.cgi?action=setConfig&NTP.TimeZone=22&NTP.TimeZoneDesc=Brasilia&NTP.Address=10.92.0.54&NTP.Enable=true",
+		userAdd:      "/cgi-bin/userManager.cgi?action=addUser&user.Name=%userAddAdmin%&user.Password=%passUserAddAdmin%A&user.Group=admin&user.Sharable=true&user.Reserved=false",
+		userAddSigeo: "/cgi-bin/userManager.cgi?action=addUser&user.Name=%userAddUser%&user.Password=%passUserAddUser%A&user.Group=user&user.Sharable=true&user.Reserved=false",
+		changePass:   "/cgi-bin/userManager.cgi?action=modifyPassword&name=admin&pwd=%senhaMaster%&pwdOld=%senhaAntiga%",
 	}
 }
 
-func configurar(ip, user, password string, cfg Configurator) error {
-	t := dac.NewTransport(user, password)
+func (m model) configurar() error {
+	t := dac.NewTransport(m.user, m.pass)
 	t.HTTPClient = &http.Client{Timeout: 2 * time.Second}
 
-	_, statusCode, err := Requisitador(t, url)
-	if err != nil {
-		return c, err
+	cfg := make(map[int]string)
+
+	switch m.response.Manufacturer {
+	case "axis":
+
+	case "dahua":
+		cfg = NewDahuaConfigurator()
+
+	default:
+	}
+
+	for k, v := range cfg {
+		switch v {
+			case
+		}
+		body, statusCode, err := Requisitador(t, url)
+		if err != nil {
+			return c, err
+		}
 	}
 
 	return nil
